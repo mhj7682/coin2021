1904ad1408caae537cf4a021df1d01e97dde047a
mhj <megi7682@gmail.com>
2021-03-23 12:54:27 +0900

    메뉴

---
+++ menu.py
@@ -0,0 +1,12 @@
+for i in range(1, 4):
+    print("밥무러 가자")
+    print("뭐물까")
+    print("1.s 2.d 3.f")
+    menu = input(str(i) + " .입력 : ")
+    if menu == '1':
+        print("1")
+    if menu == '2':
+        print("2")
+    if menu == '3':
+        print("3")
+    
